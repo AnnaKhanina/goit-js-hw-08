83f1e14a7fc070bdb723d26c44e1e9790d1fadb0
Anna Khanina <77342940+AnnaKhanina@users.noreply.github.com>
2022-06-22 22:26:05 +0300

    15

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,13 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE = "videoplayer-current-time";
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then().catch();
+
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE)).then().catch();
 
 player.on('timeupdate', throttle((data) => {
     localStorage.setItem("videoplayer-current-time", data.seconds);
