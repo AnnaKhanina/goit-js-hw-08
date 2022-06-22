1dcc5f705cb28c7f961e6d13328beb4ef029188b
Anna Khanina <77342940+AnnaKhanina@users.noreply.github.com>
2022-06-22 21:20:11 +0300

    (((

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,40 +1,40 @@
-// import Player from '@vimeo/player';
-// import throttle from 'lodash.throttle';
-
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-
-
-// player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then().catch();
-
-// player.on('timeupdate', throttle((data) => {
-//     localStorage.setItem("videoplayer-current-time", data.seconds);
-// }, 1000));
-
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const LOCALSTORAGE_KEY = 'videoplayer-current-time';
-const iframe = document.querySelector('#vimeo-player');    
+const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle(clickPlay, 1000));
-player.on('ended', ended);
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then().catch();
+
+player.on('timeupdate', throttle((data) => {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+}, 1000));
+
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+
+// const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+// const iframe = document.querySelector('#vimeo-player');    npm 
+// const player = new Player(iframe);
+
+// player.on('timeupdate', throttle(clickPlay, 1000));
+// player.on('ended', ended);
 
 
-function clickPlay( { seconds } ){
-    const currentTimeValue = JSON.stringify(seconds);
-    localStorage.setItem(LOCALSTORAGE_KEY, currentTimeValue);
-    };
+// function clickPlay( { seconds } ){
+//     const currentTimeValue = JSON.stringify(seconds);
+//     localStorage.setItem(LOCALSTORAGE_KEY, currentTimeValue);
+//     };
 
-function ended(){
-    player.setCurrentTime(0).then(function(){})
-    };
+// function ended(){
+//     player.setCurrentTime(0).then(function(){})
+//     };
 
-const playFromCurrentTime = function (){
-    const getCurrentStorageTime = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-    player.setCurrentTime(getCurrentStorageTime).then(function(){});
-    };
+// const playFromCurrentTime = function (){
+//     const getCurrentStorageTime = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+//     player.setCurrentTime(getCurrentStorageTime).then(function(){});
+//     };
 
-    playFromCurrentTime();
+//     playFromCurrentTime();
 
