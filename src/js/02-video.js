df6ee5668689fc6bba346c53e466459b56445dfc
Anna Khanina <77342940+AnnaKhanina@users.noreply.github.com>
2022-06-22 20:40:20 +0300

    new bugs

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,7 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('#vimeo-player');
+const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 
@@ -10,3 +10,4 @@ player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then().c
 player.on('timeupdate', throttle((data) => {
     localStorage.setItem("videoplayer-current-time", data.seconds);
 }, 1000));
+
