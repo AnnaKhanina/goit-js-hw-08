3f45f67ea5d33c6b963f93c84355fbe6f54e2a39
Anna Khanina <77342940+AnnaKhanina@users.noreply.github.com>
2022-06-22 21:47:29 +0300

    14

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,33 +9,4 @@ player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then().c
 
 player.on('timeupdate', throttle((data) => {
     localStorage.setItem("videoplayer-current-time", data.seconds);
-    return player.play();
 }, 1000));
-
-// import Player from '@vimeo/player';
-// import throttle from 'lodash.throttle';
-
-// const LOCALSTORAGE_KEY = 'videoplayer-current-time';
-// const iframe = document.querySelector('#vimeo-player');    npm 
-// const player = new Player(iframe);
-
-// player.on('timeupdate', throttle(clickPlay, 1000));
-// player.on('ended', ended);
-
-
-// function clickPlay( { seconds } ){
-//     const currentTimeValue = JSON.stringify(seconds);
-//     localStorage.setItem(LOCALSTORAGE_KEY, currentTimeValue);
-//     };
-
-// function ended(){
-//     player.setCurrentTime(0).then(function(){})
-//     };
-
-// const playFromCurrentTime = function (){
-//     const getCurrentStorageTime = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-//     player.setCurrentTime(getCurrentStorageTime).then(function(){});
-//     };
-
-//     playFromCurrentTime();
-
